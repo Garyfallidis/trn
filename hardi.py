72b8edb583076a9701eeb460cc2a76c9c24ace99
Eleftherios Garyfallidis <garyfallidis@gmail.com>
2012-03-13 09:37:40 +0000

    hardi modified

--- hardi.py
+++ hardi.py
@@ -14,51 +14,6 @@ from scipy.optimize import fmin as fmin_powell
 from scipy.optimize import leastsq
 from time import time
 
-def SticksAndBall(bvals,gradients,d=0.0015,S0=100,angles=[(0,0),(90,0)],fractions=[35,35],snr=20):
-    """ Simulating the signal for a Sticks & Ball model 
-    
-    Based on the paper by Tim Behrens, H.J. Berg, S. Jbabdi, "Probabilistic Diffusion Tractography with multiple fiber orientations
-    what can we gain?", Neuroimage, 2007. 
-    
-    Parameters
-    -----------
-    bvals : array, shape (N,)
-    gradients : array, shape (N,3) also known as bvecs
-    d : diffusivity value 
-    S0 : unweighted signal value
-    angles : array (K,2) list of polar angles (in degrees) for the sticks
-        or array (K,3) with sticks as Cartesian unit vectors and K the number of sticks
-    fractions : percentage of each stick
-    snr : signal to noise ration assuming gaussian noise. Provide None for no noise.
-    
-    Returns
-    --------
-    S : simulated signal
-    sticks : sticks in cartesian coordinates 
-    
-    """
-    
-    fractions=[f/100. for f in fractions]    
-    f0=1-np.sum(fractions)    
-    S=np.zeros(len(gradients))
-    
-    angles=np.array(angles)
-    if angles.shape[-1]==3:
-        sticks=angles
-    if angles.shape[-1]==2:
-        sticks=[ sphere2cart(1,np.deg2rad(pair[0]),np.deg2rad(pair[1]))  for pair in angles]    
-        sticks=np.array(sticks)
-    
-    for (i,g) in enumerate(gradients[1:]):
-        S[i+1]=f0*np.exp(-bvals[i+1]*d)+ np.sum([fractions[j]*np.exp(-bvals[i+1]*d*np.dot(s,g)**2) for (j,s) in enumerate(sticks)])
-        S[i+1]=S0*S[i+1]    
-    S[0]=S0    
-    if snr!=None:
-        std=S0/snr
-        S=S+np.random.randn(len(S))*std
-    
-    return S,sticks
-
 def SingleTensor(bvals,gradients,S0,evals,evecs,snr=None):
     """ Simulated signal with a Single Tensor
      
@@ -119,9 +74,10 @@ def ODF(vecs,mf,mevals,mevecs):
 def lambda_ranges():
     print 'max', 1*10**(-3),'to',2*10**(-3)
     print 'other', 0.1*10**(-3),'to',0.6*10**(-3)
-    lmin=np.linspace(0.1,0.6,20)*10**(-3)
-    lmax=np.linspace(1,2,20)*10**(-3)
-    return lmax,lmin 
+    lmin=np.linspace(0.1,0.6,10)*10**(-3)
+    lmax=np.linspace(1,2,10)*10**(-3)
+    f=np.linspace(0.1,1,10)
+    return lmax,lmin,f 
 
 def count_peaks(PK):
     return np.sum(PK>0,axis=-1)
@@ -324,6 +280,8 @@ def save_for_mat(M,R):
     pass
 
 if __name__ == '__main__':
+
+    stop
 
     data,bvals,bvecs,odf_sphere=load_data('train','SF','30')#'3D_SF'
 
