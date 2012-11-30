1a88790fec0a70dc99d71252a5291b6c9a0b718e
Eleftherios Garyfallidis <garyfallidis@gmail.com>
2012-11-29 20:47:39 -0500

    steam_dsi updated

--- steam_dsi.py
+++ steam_dsi.py
@@ -9,30 +9,38 @@ from dipy.reconst.dsi import half_to_full_qspace
 
 if __name__ == '__main__':
 
-    path = '/home/eg309/Data/PROC_MR10032/subj_03/101_32/1312211075232351192010121313490254679236085ep2dadvdiffDSI10125x25x25STs004a001'    
-   
-    img = nib.load(path+'.nii')
+    fname = '/home/eleftherios/Data/trento_processed/subj_01/101_32/raw'
+    img = nib.load(fname + 'bet.nii.gz')
     data = img.get_data()
     affine = img.get_affine()
-    bvals = np.loadtxt(path+'.bval')
-    bvecs = np.loadtxt(path+'.bvec').T
-    
+    bvals = np.loadtxt(fname + '.bval')
+    bvecs = np.loadtxt(fname + '.bvec').T
+
     gtab = gradient_table(bvals, bvecs)
-
-
     sphere = get_sphere('symmetric724')
 
-    #data_roi = data[48-20:48+20,48-20:48+20,35,:]
-    #data_roi = data_roi[:,:,None,:]     
-    #data_roi = data_roi.astype('f8')
+    w=10
+    data_roi = data[:, :, 32]
+    #data_roi = data[48-w:48+w, 48-w:48+w, 32] #27-w:27+w,:]
+    data_roi = data_roi[:,:,None,:]
+    data = data_roi.astype('f8')
+
     data, gtab = half_to_full_qspace(data, gtab)
-        
+
     ds = DiffusionSpectrumModel(gtab)
     ds.direction_finder.config(sphere=sphere, 
                                 min_separation_angle=25, 
                                 relative_peak_threshold=.35)
     dsfit = ds.fit(data)
-    ODF = dsfit.odf(sphere)
+    #directions = dsfit.directions
+    gfa = dsfit.gfa
+    peak_values = dsfit.peak_values
+    peak_indices = dsfit.peak_indices
+    
+    sums=[]
+    for d in dsfit.directions.ravel():
+        sums.append(d.shape[0])
+    #ODF = dsfit.odf(sphere)
     #PDF = dsfit.pdf()
 
     #from dipy.viz._show_odfs import show_odfs
