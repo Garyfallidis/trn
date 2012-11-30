6a2bd465c6d0839a8cddf220c149c9288c7e4b74
Eleftherios Garyfallidis <garyfallidis@gmail.com>
2012-11-30 16:55:41 -0500

    update steam_dsi

--- steam_dsi.py
+++ steam_dsi.py
@@ -5,7 +5,7 @@ from dipy.data import get_data, get_sphere
 from dipy.reconst.dsi import DiffusionSpectrumModel, project_hemisph_bvecs
 from dipy.core.gradients import gradient_table
 from dipy.reconst.dsi import half_to_full_qspace
-
+from dipy.reconst.gqi import GeneralizedQSamplingModel
 
 if __name__ == '__main__':
 
@@ -19,31 +19,45 @@ if __name__ == '__main__':
     gtab = gradient_table(bvals, bvecs)
     sphere = get_sphere('symmetric724')
 
+    """
     w=10
     data_roi = data[:, :, 32]
     #data_roi = data[48-w:48+w, 48-w:48+w, 32] #27-w:27+w,:]
     data_roi = data_roi[:,:,None,:]
     data = data_roi.astype('f8')
+    """
 
+    S0 = data[..., 0]
+    mask = S0 > 50
+
+    gq = GeneralizedQSamplingModel(gtab)
+    gq.direction_finder.config(sphere=sphere, 
+                                min_separation_angle=25, 
+                                relative_peak_threshold=.35)
+    gqfit = gq.fit(data, mask)
+    gfa = gqfit.gfa
+    peak_values = gqfit.peak_values
+    peak_indices = gqfit.peak_indices
+    qa = gqfit.qa
+    #ODF = gqfit.odf(sphere)
+
+
+    """
     data, gtab = half_to_full_qspace(data, gtab)
-
     ds = DiffusionSpectrumModel(gtab)
     ds.direction_finder.config(sphere=sphere, 
                                 min_separation_angle=25, 
                                 relative_peak_threshold=.35)
-    dsfit = ds.fit(data)
+
+    dsfit = ds.fit(data, mask)
     #directions = dsfit.directions
     gfa = dsfit.gfa
     peak_values = dsfit.peak_values
     peak_indices = dsfit.peak_indices
-    
-    sums=[]
-    for d in dsfit.directions.ravel():
-        sums.append(d.shape[0])
     #ODF = dsfit.odf(sphere)
     #PDF = dsfit.pdf()
 
     #from dipy.viz._show_odfs import show_odfs
     #show_odfs(ODF, (sphere.vertices, sphere.faces))
-
+    """
 
